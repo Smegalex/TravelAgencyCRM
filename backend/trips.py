5ecf33aab5e376ea7b02ed8b7299a9754602e6fd
Smegalex <smegalsuper@gmail.com>
2024-12-18 19:53:38 +0200

    trips backend done

--- backend/trips.py
+++ backend/trips.py
@@ -1,49 +1,46 @@
 from flask import Blueprint, jsonify, request
-from db_cursor import select_records, insert_new_record
+from db_cursor import select_records, insert_new_record, get_last_id, update_row_in_table, delete_row_from_table
 
 #ID, NAME, IDPLACE
 table_name = "trips"
-trips = select_records(table_name)
 
-def get_trip_from_DB(trip_id):
+def get_trip_from_DB(trip_id=None):
     return select_records(table_name, trip_id)
-
-print(trips)
-next_id = trips[-1][0] + 1 
 
 trips_bp = Blueprint('trips', __name__)
 
 @trips_bp.route('/trips', methods=['GET'])
 def get_trips():
-    return jsonify(trips)
+    return jsonify(get_trip_from_DB())
 
 @trips_bp.route('/trips/<int:trip_id>', methods=['GET'])
 def get_trip(trip_id):
-    return jsonify(trips[trip_id])
+    return jsonify(get_trip_from_DB(trip_id))
 
 @trips_bp.route('/trips', methods=['POST'])
 def add_trip():
+    global next_id
     trip_data = request.json
     new_trip = {
         "name": trip_data["name"],
         "idplace": trip_data["idplace"]
     }
     insert_new_record(table_name, new_trip)
-    next_id += 1
-    trips.append(get_trip_from_DB(next_id))
-    return jsonify(new_trip), 201
+    created_trip = get_trip_from_DB(get_last_id(table_name))[0]
+    return jsonify(created_trip), 201
 
 @trips_bp.route('/trips/<int:trip_id>', methods=['PUT'])
 def update_trip(trip_id):
     trip_data = request.json
-    trip = next((s for s in trips if s["id"] == trip_id), None)
+    trips = get_trip_from_DB()
+    trip = next((s for s in trips if s[0] == trip_id), None)
+    print(trip)
     if not trip:
         return jsonify({"error": "trip not found"}), 404
-    trip.update({"name": trip_data["name"], "idplace": trip_data["idplace"]})
-    return jsonify(trip)
+    update_row_in_table(table_name, trip_id, {"name": trip_data["name"], "idplace": trip_data["idplace"]})
+    return jsonify(get_trip_from_DB(trip_id))
 
 @trips_bp.route('/trips/<int:trip_id>', methods=['DELETE'])
 def delete_trip(trip_id):
-    global trips
-    trips = [s for s in trips if s["id"] != trip_id]
+    delete_row_from_table(table_name, trip_id)
     return '', 204
