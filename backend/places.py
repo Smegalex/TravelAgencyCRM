8dcf50417c80fd11e51d127a35e1c331ee4351b6
rshvtss <r.shvetsss@gmail.com>
2024-12-19 00:32:34 +0200

    changed places.py and .json
    json міняв, бо відмовлялось npm run dev виконувати

--- backend/places.py
+++ backend/places.py
@@ -1,38 +1,56 @@
 from flask import Blueprint, jsonify, request
+from db_cursor import select_records, insert_new_record, get_last_id, update_row_in_table, delete_row_from_table
 
-places = []
-next_id = 1
+#ID, NAME, IDPLACE
+table_name = "places"
+
+def get_place_from_DB(place_id=None):
+    return select_records(table_name, place_id)
 
 places_bp = Blueprint('places', __name__)
 
 @places_bp.route('/places', methods=['GET'])
 def get_places():
-    return jsonify(places)
+    return jsonify(get_place_from_DB())
+
+@places_bp.route('/places/<int:place_id>', methods=['GET'])
+def get_place(place_id):
+    return jsonify(get_place_from_DB(place_id))
 
 @places_bp.route('/places', methods=['POST'])
 def add_place():
     global next_id
     place_data = request.json
     new_place = {
-        "id": next_id,
         "name": place_data["name"],
-        "age": place_data["age"]
+        "country": place_data["country"],
+        "coordinates": place_data["coordinates"],
+        "description": place_data["description"]
     }
-    places.append(new_place)
-    next_id += 1
-    return jsonify(new_place), 201
+    insert_new_record(table_name, new_place)
+    created_place = get_place_from_DB(get_last_id(table_name))[0]
+    return jsonify(created_place), 201
 
 @places_bp.route('/places/<int:place_id>', methods=['PUT'])
 def update_place(place_id):
     place_data = request.json
-    place = next((s for s in places if s["id"] == place_id), None)
+    places = get_place_from_DB()
+    place = next((s for s in places if s[0] == place_id), None)
     if not place:
         return jsonify({"error": "place not found"}), 404
-    place.update({"name": place_data["name"], "age": place_data["age"]})
-    return jsonify(place)
+    update_row_in_table(
+        table_name,
+        place_id,
+        {
+            "name": place_data["name"],
+            "country": place_data["country"],
+            "coordinates": place_data["coordinates"],
+            "description": place_data["description"]
+        }
+    )
+    return jsonify(get_place_from_DB(place_id))
 
 @places_bp.route('/places/<int:place_id>', methods=['DELETE'])
 def delete_place(place_id):
-    global places
-    places = [s for s in places if s["id"] != place_id]
+    delete_row_from_table(table_name, place_id)
     return '', 204
