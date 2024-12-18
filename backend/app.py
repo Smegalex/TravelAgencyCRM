aa19fa3f593fb969d57bc4050a3c5f77d579e601
Smegalex <smegalsuper@gmail.com>
2024-12-18 19:54:45 +0200

    unnecessary lines removed

--- backend/app.py
+++ backend/app.py
@@ -6,18 +6,6 @@ from clients import clients_bp
 from places import places_bp
 from bookings import bookings_bp
 from trips import trips_bp
-from db_cursor import get_db_cursor
-
-conn, cursor = get_db_cursor()
-cursor.execute("SELECT Table_name FROM user_tables")
-for row in cursor:
-    print(row)
-
-# Close the cursor and connection
-cursor.close()
-conn.close()
-
-
 
 app = Flask(__name__)
 CORS(app)
