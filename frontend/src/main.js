2c07d1fb1e7ef7e7967cad44f3c532570255b307
Smegalex <smegalsuper@gmail.com>
2024-12-20 20:06:54 +0200

    Primevue theme preset fixed + CustomFooter component initialized

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,16 +2,22 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import CustomHeader from "./components/CustomHeader.vue";
-	
+import CustomFooter from "./components/CustomFooter.vue";
+import Nora from "@primevue/themes/nora";
+import "primeflex/primeflex.css";
+import "@/styles/styles.scss";
+
 const app = createApp(App);
 
 app.use(PrimeVue, {
 	theme: {
-		preset: "Nora",
+		preset: Nora,
 	},
 });
 
+
 app.component("CustomHeader", CustomHeader);
+app.component("CustomFooter", CustomFooter);
 
 app.mount("#app");
 export default app;
