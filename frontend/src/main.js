37571240fb878572d5a681971a601e4ccf7c722d
Smegalex <smegalsuper@gmail.com>
2024-12-21 16:27:23 +0200

    new component and theme adjustment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import CustomHeader from "./components/CustomHeader.vue";
 import CustomFooter from "./components/CustomFooter.vue";
+import UniversalModal from "./pages/modals/UniversalModal.vue";
 import Nora from "@primevue/themes/nora";
 import "primeflex/primeflex.css";
 import "@/styles/styles.scss";
@@ -12,12 +13,15 @@ const app = createApp(App);
 app.use(PrimeVue, {
 	theme: {
 		preset: Nora,
+		options: {
+			prefix: "",
+		},
 	},
 });
 
-
 app.component("CustomHeader", CustomHeader);
 app.component("CustomFooter", CustomFooter);
+app.component("UniversalModal", UniversalModal);
 
 app.mount("#app");
 export default app;
